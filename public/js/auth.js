b8ee0afcca09738b1f097f5c5971e007aab1ead5
dananum-25 <sh2439.kim@gmail.com>
2026-02-17 11:02:03 +0900

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbz1QnNlStdQYSaLu6x-LjcfbhW_mr-G9imRieFc56tqJ15UZchDDOdUrqpjNCO1ImOQ/exec";
+"https://script.google.com/macros/s/AKfycbx6NjF9IVzW0eA0fE_q54B8wRQMPq8BivT3snTuNfDTTc-ggaYqoRw7AMqrqOeT5Kz_9A/exec";
 /* =========================================
    AUTH GUARD + LOGIN (auth.js)
 ========================================= */
