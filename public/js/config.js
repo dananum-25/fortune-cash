36978474b62f7464dbd101527a40e85821de7b59
dananum-25 <sh2439.kim@gmail.com>
2026-02-20 06:22:00 +0900

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+// public/js/config.js
+window.APP_CONFIG = window.APP_CONFIG || {};
+window.APP_CONFIG.API_URL =
+  "https://script.google.com/macros/s/AKfycbxV49VtrO-fV68nmPLsZFYZnDp6F8OwDFGaOe3Kj_Syi6LW7znJ5dHlHx5ZgK3uNlClZw/exec";
