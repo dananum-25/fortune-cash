db9ce25060dfa06e1c7e083a855ec95b475727e2
dananum-25 <sh2439.kim@gmail.com>
2026-02-20 12:37:09 +0900

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,3 +2,8 @@
 window.APP_CONFIG = window.APP_CONFIG || {};
 window.APP_CONFIG.API_URL =
   "https://script.google.com/macros/s/AKfycbxV49VtrO-fV68nmPLsZFYZnDp6F8OwDFGaOe3Kj_Syi6LW7znJ5dHlHx5ZgK3uNlClZw/exec";
+
+// ✅ 어디서든 안전하게 쓰는 공통 함수
+window.getApiUrl = window.getApiUrl || function(){
+  return window.APP_CONFIG?.API_URL || "";
+};
