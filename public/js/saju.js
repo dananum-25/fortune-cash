6c64d94c043b28fbd01107e6c0d7c3b1b781524d
dananum-25 <sh2439.kim@gmail.com>
2026-03-01 11:44:00 +0900

    Update saju.js

--- public/js/saju.js
+++ public/js/saju.js
@@ -287,7 +287,7 @@ function calculateSaju(){
     return;
   }
 
-  const birthDate = parseYmdKST(birth);
+  const birthDate = parseYmdLocal(birth);
   if(!birthDate){
     alert("생년월일 형식이 이상합니다. 다시 로그인/저장 후 시도해주세요.");
     return;
