7ef56dff4c4ae69593dc82d892c93826a816cf64
dananum-25 <sh2439.kim@gmail.com>
2026-03-01 22:00:27 +0900

    Update saju.ui.js

--- public/js/saju.ui.js
+++ public/js/saju.ui.js
@@ -846,8 +846,8 @@ function calculateSaju(){
 
   const yearPillar  = getYearPillar(year);
   const monthPillar = getMonthPillar(birthDate);
-  const dayPillar = getDayPillar(birth);  // birth는 YYYY-MM-DD 문자열
-const hourPillar = getHourPillar(dayPillar, hour);
+  const dayPillar = window.SajuCore.getDayPillar(birth);
+const hourPillar = window.SajuCore.getHourPillar(dayPillar, hour);
   const hourPillar  = getHourPillar(dayPillar, hour);
 
   const pillars = [yearPillar, monthPillar, dayPillar, hourPillar];
