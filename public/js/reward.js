35b2ed31b328d48f29fdd3c8ffe316f5e4f3e1cd
dananum-25 <sh2439.kim@gmail.com>
2026-03-08 11:47:22 +0900

    Update reward.js

--- public/js/reward.js
+++ public/js/reward.js
@@ -5,7 +5,9 @@ async function rewardContent(type){
   const allowedTypes = ["attendance", "fortune_view", "share_event"];
   const safeType = allowedTypes.includes(type) ? type : "fortune_view";
 
-  await fetch(API_URL, {
+  if(!window.API_URL) return;
+
+await fetch(API_URL,{
     method: "POST",
     headers: {
       "Content-Type": "application/json"
