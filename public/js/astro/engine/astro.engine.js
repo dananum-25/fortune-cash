2948d4e2b75bf3e205094ad76fe0578a3d45c9b1
dananum-25 <sh2439.kim@gmail.com>
2026-03-12 03:13:12 +0900

    Create astro.engine.js

---
+++ public/js/astro/engine/astro.engine.js
@@ -0,0 +1,45 @@
+// /js/astro/engine/astro.engine.js
+
+import {
+  ASTRO_DEFAULT_BIRTH,
+  ASTRO_DEFAULT_TIME,
+  ASTRO_DEFAULT_TIMEZONE
+} from "/js/astro/core/astro.constants.js";
+
+import {
+  buildNatalChart
+} from "/js/astro/profile/astro.natal.js";
+
+import {
+  buildPersonalityProfile
+} from "/js/astro/profile/astro.personality.js";
+
+export function buildAstroBaseProfile(input = {}){
+  const birthDate = input.birthDate || ASTRO_DEFAULT_BIRTH;
+  const birthTime = input.birthTime || ASTRO_DEFAULT_TIME;
+  const timezone = input.timezone || ASTRO_DEFAULT_TIMEZONE;
+  const birthPlaceText = input.birthPlaceText || "Seoul";
+  const lat = Number(input.lat || 37.5665);
+  const lng = Number(input.lng || 126.9780);
+
+  const natal = buildNatalChart({
+    birthDate,
+    birthTime,
+    timezone,
+    birthPlaceText,
+    lat,
+    lng
+  });
+
+  if(!natal){
+    return null;
+  }
+
+  const personality = buildPersonalityProfile(natal);
+
+  return {
+    birth: natal.birth,
+    natal,
+    personality
+  };
+}
